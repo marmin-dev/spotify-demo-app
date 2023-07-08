af406618363f7c4c311b5fe27ce84eda9a5d3f94
marmin-dev <marmin.dev@gmail.com>
2023-07-08 13:08:37 +0900

    spotify-demo-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import SpotifyWebApi from "spotify-web-api-js";
+import SpotifyPlayer from "react-spotify-web-playback";
+
+const spotifyApi = new SpotifyWebApi();
+
+const getTokenFromUrl = () => {
+  const search = window.location.hash.substring(1); // 쿼리 부분 가져오기
+  const params = new URLSearchParams(search); // URLSearchParams 객체 생성
+  let accessToken = params.toString().split("=");
+  accessToken = accessToken[1].split("&")[0];
+  console.log(accessToken);
+  return accessToken;
+};
 
 function App() {
+  const [spotifyToken, setSpotifyToken] = useState("");
+  const [newPlaying, setNewPlaying] = useState({});
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (window.location.hash.substring(1)) {
+      console.log("this is what we derived from url", getTokenFromUrl());
+      const spotifyToken = getTokenFromUrl();
+      // window.location.hash = "";
+      console.log(spotifyToken);
+      if (spotifyToken) {
+        setSpotifyToken(spotifyToken);
+        spotifyApi.setAccessToken(spotifyToken);
+
+        spotifyApi.getMe().then((user) => {
+          console.log(user);
+        });
+        // use spotify api
+        setLoggedIn(true);
+      }
+    }
+  });
+
+  const getNowPlaying = () => {
+    spotifyApi.getMyCurrentPlaybackState().then((response) => {
+      console.log(response);
+      setNewPlaying({
+        name: response.item.name,
+        albumArt: response.item.album.images[0].url,
+      });
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!loggedIn && <a href="http://localhost:8888">Login to spotify</a>}
+      {loggedIn && (
+        <SpotifyPlayer
+          token={spotifyToken}
+          uris={["spotify:album:6Z1zv6Hw9bdvSoxI5uYk2h"]}
+          styles={{
+            activeColor: "#fff",
+            bgColor: "#333",
+            color: "#fff",
+            loaderColor: "#fff",
+            sliderColor: "#1cb954",
+            trackArtistColor: "#ccc",
+            trackNameColor: "#fff",
+          }}
+        />
+      )}
+      {loggedIn && (
+        <>
+          <div>Now Playing {newPlaying.name}</div>
+          <div>
+            <img src={newPlaying.albumArt} style={{ height: 150 }} />
+          </div>
+        </>
+      )}
+      {loggedIn && (
+        <button onClick={() => getNowPlaying()}>Get Now Playing</button>
+      )}
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=$response_type=code&redirect_uri=http://localhost:3000&scope=";
+
+const Login = () => {
+  return <div></div>;
+};
+
+export default Login;
